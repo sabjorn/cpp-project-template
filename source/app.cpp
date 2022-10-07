b7073192db2c93c894a272b16ea0f26054ef6e9e
sabjorn <info@sabjorn.net>
2022-10-07 11:50:52 -0700

    small update

--- source/app.cpp
+++ source/app.cpp
@@ -11,15 +11,20 @@ int main()
     {
         auto gain = Gain(2.0);
         auto db = static_cast<Db<double>>(gain);
+        auto multiply = gain * db;
 
-        cout << "gain : " << gain.getValue() << " db: " << db.getValue() << "\n"; // static_cast<Gain>(amp).getValue() << "\n";
+        cout << "gain : " << static_cast<double>(gain) << " db: " << static_cast<double>(db) << " multiply: " << static_cast<double>(multiply) << "\n";
+        cout << "gain * gain: " << gain * gain << "\n";
     }
 
     {
         auto db = Db(-6.0);
         auto gain = static_cast<Gain<double>>(db);
+        auto multiply = db * gain;
 
-        cout << "gain : " << gain.getValue() << " db: " << db.getValue() << "\n"; // static_cast<Gain>(amp).getValue() << "\n";
+        cout <<  "db: " << static_cast<double>(db) << " gain: " << static_cast<double>(gain) << " multiply: " << static_cast<double>(multiply) << "\n";
+        cout << "db * db: " << db * db << "\n";
     }
+
     return 0;
 }
